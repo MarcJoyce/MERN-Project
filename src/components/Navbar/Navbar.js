ff236e7bebd42fd004ff3ca312c8032555a1b3e7
Marc Joyce <marcjoyce90@gmail.com>
2021-05-15 12:43:11 +0100

    made changes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -51,7 +51,7 @@ const Navbar = () => {
           <Button variant="contained" className={classes.logout} color="secondary" onClick={handleLogout}>Logout</Button>
         </div>
       ) : (
-          <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
+          <Button component={Link} to="/MERN-Project/auth" variant="contained" color="primary">Sign In</Button>
       )}
     </Toolbar>
     </AppBar>
