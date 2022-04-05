af9d379fb6c8e6160a7604beec1cc7fc143b163b
Marc Joyce <Marc.Joyce@shelbyfinance.com>
2022-04-05 09:45:15 +0100

    [CORS]

--- src/api/index.js
+++ src/api/index.js
@@ -1,23 +1,29 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://memories-project-mern-test.herokuapp.com", headers: {
-  'Content-Type': 'application/json',
-  Accept: 'application/json'
-} })
+const API = axios.create({
+  baseURL: "https://memories-project-mern-test.herokuapp.com",
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/json",
+  },
+});
 
 API.interceptors.request.use((req) => {
-  if (localStorage.getItem('profile')) {
-    req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`
+  if (localStorage.getItem("profile")) {
+    req.headers.Authorization = `Bearer ${
+      JSON.parse(localStorage.getItem("profile")).token
+    }`;
   }
 
   return req;
 });
 
-export const fetchPosts = () => API.get('/posts');
-export const createPost = (newPost) => API.post('/posts', newPost);
+export const fetchPosts = () => API.get("/posts");
+export const createPost = (newPost) => API.post("/posts", newPost);
 export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
-export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
+export const updatePost = (id, updatedPost) =>
+  API.patch(`/posts/${id}`, updatedPost);
 export const deletePost = (id) => API.delete(`/posts/${id}`);
 
-export const signIn = (formData) => API.post('/user/signin', formData);
-export const signUp = (formData) => API.post('/user/signup', formData);
+export const signIn = (formData) => API.post("/user/signin", formData);
+export const signUp = (formData) => API.post("/user/signup", formData);
